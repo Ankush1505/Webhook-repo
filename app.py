14184a711caf88a0d58cbb6caaaf5c395bd5fb58
Ankush1505 <ankushparsad2002@gmail.com>
2026-04-19 02:18:22 +0530

    Fixed MongoDB SSL with certifi and added Swagger UI

--- app.py
+++ app.py
@@ -4,53 +4,76 @@ from pymongo import MongoClient
 from datetime import datetime
 from dateutil import parser
 import os
+from flasgger import Swagger
 
 app = Flask(__name__)
+swagger = Swagger(app)
 
-# TODO: In production, move this URI to an environment variable (.env) for security.
 MONGO_URI = "mongodb+srv://ankushsarsswat2002_db_user:ankush123@cluster0.wfhnmn0.mongodb.net/?appName=Cluster0" 
 
-# Connect to MongoDB
-# We use MongoDB to store raw JSON payloads from GitHub without schema constraints
-client = MongoClient(MONGO_URI, tls=True, tlsAllowInvalidCertificates=True)
+client = MongoClient(MONGO_URI, tlsCAFile=certifi.where())
 db = client['techstax_db'] 
 collection = db['events']
 
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    
-    #Endpoint to receive GitHub Webhook events.
-    #Handles 'Push' and 'Pull Request' (Merge) events.
+
+    """
+    GitHub Webhook Listener Endpoint
+    This endpoint receives POST payloads directly from GitHub events.
+    ---
+    tags:
+      - Webhook Operations
+    consumes:
+      - application/json
+    parameters:
+      - in: body
+        name: payload
+        required: true
+        description: The JSON payload sent by GitHub
+        schema:
+          type: object
+          properties:
+            zen:
+              type: string
+              example: "Responsive is better than fast."
+            repository:
+              type: object
+              properties:
+                name:
+                  type: string
+                  example: "TechStaX-Webhook"
+    responses:
+      200:
+        description: Webhook payload received and processed successfully.
+      400:
+        description: Invalid JSON payload.
+    """
     
     data = request.json
 
-    # Validate that payload exists.------
     if not data:
         return jsonify({"msg": "No data received"}), 400
     
-
-    # Idempotancy for webhook-------
     delivery_id = request.headers.get('X-Github-Delivery')
 
     if collection.find_one({"delivery_id" : delivery_id}):
         print(f"Duplicate webhooks Blocked : {delivery_id}")
         return jsonify({"Reason" : "Duplicate Ignored"}), 200
 
-    # Identify the event type from headers.------
+   
     event_type = request.headers.get('X-GitHub-Event')
 
     record = {}
 
-    # Handle PUSH Event.
+   
     if event_type == 'push':
         author = data['pusher']['name']
         to_branch = data['ref'].split('/')[-1] 
         timestamp = parser.parse(data['head_commit']['timestamp'])
         
-        
         msg = f'"{author}" pushed to "{to_branch}" on {timestamp.strftime("%d %B %Y - %I:%M %p UTC")}'
-        
         record = {
             "type": "PUSH",
             "author": author,
@@ -59,7 +82,7 @@ def webhook():
             "message": msg
         }
 
-    # Handle PULL_REQUEST Event (Check for Merge)
+    
     elif event_type == 'pull_request':
         action = data['action']
         pr = data['pull_request']
@@ -68,7 +91,7 @@ def webhook():
         to_branch = pr['base']['ref']
         timestamp = parser.parse(pr['updated_at'])
 
-        # Case A: Merge (Closed + Merged = True)
+        
         if action == 'closed' and pr['merged'] == True:
             msg = f'"{author}" merged branch "{from_branch}" to "{to_branch}" on {timestamp.strftime("%d %B %Y - %I:%M %p UTC")}'
             record = {
@@ -80,7 +103,7 @@ def webhook():
                 "message": msg
             }
         
-        # Case B: Standard PR Action (Opened/Edited/Reopened)
+        
         elif action in ['opened', 'edited', 'reopened']:
             msg = f'"{author}" submitted a pull request from "{from_branch}" to "{to_branch}" on {timestamp.strftime("%d %B %Y - %I:%M %p UTC")}'
             record = {
@@ -92,13 +115,13 @@ def webhook():
                 "message": msg
             }
 
-    # Save to Database if a valid record was created.
+   
     if record:
         record['delivery_id'] = delivery_id
         collection.insert_one(record)
         print(f" Saved Event: {record['message']}")
     
-    return jsonify({"status": "success"}), 200
+    return jsonify({"status": "success", "message": "Event logged"}), 200
 
 
 
@@ -115,5 +138,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    #Running on port 50001 to avoid conflicts.
+    
     app.run(debug=True, port=50001)
